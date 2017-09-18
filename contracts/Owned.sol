9299145331715cfaf336b61feb76d651e829fca9
keorn <pczaban@gmail.com>
2017-09-18 15:25:28 +0100

    new sets, reuse code

--- contracts/Owned.sol
+++ contracts/Owned.sol
@@ -1,16 +1,16 @@
 //! Owned contract.
-//! By Gav Wood (Ethcore), 2016.
+//! Copyright Parity Technologies Ltd (UK), 2016.
+//! By Gav Wood, 2016.
 //! Released under the Apache Licence 2.
 
-pragma solidity ^0.4.6;
+pragma solidity ^0.4.15;
 
 contract Owned {
-    modifier only_owner { if (msg.sender != owner) return; _; }
+	modifier only_owner { require(msg.sender != owner); _; }
 
-    event NewOwner(address indexed old, address indexed current);
+	event NewOwner(address indexed old, address indexed current);
 
-    function setOwner(address _new) only_owner { NewOwner(owner, _new); owner = _new; }
+	function setOwner(address _new) only_owner { NewOwner(owner, _new); owner = _new; }
 
-    address public owner = msg.sender;
+	address public owner = msg.sender;
 }
-
