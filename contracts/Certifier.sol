70bd9bd085a32e5a0d5e5cee352b30a275c94875
keorn <pczaban@gmail.com>
2017-10-20 15:03:53 +0100

    smsverification

--- contracts/Certifier.sol
+++ contracts/Certifier.sol
@@ -2,13 +2,13 @@
 //! By Gav Wood (Ethcore), 2016.
 //! Released under the Apache Licence 2.
 
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.17;
 
 contract Certifier {
 	event Confirmed(address indexed who);
 	event Revoked(address indexed who);
-	function certified(address _who) constant returns (bool);
-	function getData(address _who, string _field) constant returns (bytes32) {}
-	function getAddress(address _who, string _field) constant returns (address) {}
-	function getUint(address _who, string _field) constant returns (uint) {}
+	function certified(address _who) constant public returns (bool);
+	function getData(address _who, string _field) constant public returns (bytes32) {}
+	function getAddress(address _who, string _field) constant public returns (address) {}
+	function getUint(address _who, string _field) constant public returns (uint) {}
 }
