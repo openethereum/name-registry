a0ca4495f48498db4cfe3394c5124af968d0288a
Wei Tang <hi@that.world>
2018-05-23 07:57:39 +0800

    Add certifier tests

---
+++ test/simple_certifier.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const SimpleCertifier = artifacts.require("./SimpleCertifier.sol");
+
+contract("SimpleRegistry", accounts => {
+  const assertThrowsAsync = async (fn, msg) => {
+    try {
+      await fn();
+    } catch (err) {
+      assert(err.message.includes(msg), "Expected error to include: " + msg);
+      return;
+    }
+    assert.fail("Expected fn to throw");
+  };
+
+   it("should allow certify an address", async () => {
+     const cert = await SimpleCertifier.deployed();
+
+     assertThrowsAsync(
+       () => cert.setDelegate(accounts[0], { from: accounts[1] }),
+       "revert"
+     );
+
+     await cert.setDelegate(accounts[0]);
+     assert.equal(await cert.certified(accounts[1]), false);
+
+     await cert.certify(accounts[1]);
+     assert.equal(await cert.certified(accounts[1]), true);
+
+     await cert.revoke(accounts[1]);
+     assert.equal(await cert.certified(accounts[1]), false);
+   });
+});
