7709de80c9043f48eadbc5fcd5b9c7e5fffc46e6
André Silva <andre.beat@gmail.com>
2018-06-01 17:15:42 +0100

    fix indent in SimpleCertifier js test

--- test/simple_certifier.js
+++ test/simple_certifier.js
@@ -13,33 +13,33 @@ contract("SimpleRegistry", accounts => {
     assert.fail("Expected fn to throw");
   };
 
-   it("should allow certify an address", async () => {
-     const cert = await SimpleCertifier.deployed();
+  it("should allow certify an address", async () => {
+    const cert = await SimpleCertifier.deployed();
 
-     assertThrowsAsync(
-       () => cert.setDelegate(accounts[0], { from: accounts[1] }),
-       "revert"
-     );
+    assertThrowsAsync(
+      () => cert.setDelegate(accounts[0], { from: accounts[1] }),
+      "revert"
+    );
 
-     await cert.setDelegate(accounts[0]);
-     assert.equal(await cert.certified(accounts[1]), false);
+    await cert.setDelegate(accounts[0]);
+    assert.equal(await cert.certified(accounts[1]), false);
 
-     await cert.certify(accounts[1]);
-     assert.equal(await cert.certified(accounts[1]), true);
+    await cert.certify(accounts[1]);
+    assert.equal(await cert.certified(accounts[1]), true);
 
-     assertThrowsAsync(
-       () => cert.revoke(accounts[1], { from: accounts[1] }),
-       "revert"
-     );
+    assertThrowsAsync(
+      () => cert.revoke(accounts[1], { from: accounts[1] }),
+      "revert"
+    );
 
-     await cert.revoke(accounts[1]);
-     assert.equal(await cert.certified(accounts[1]), false);
+    await cert.revoke(accounts[1]);
+    assert.equal(await cert.certified(accounts[1]), false);
 
-     assertThrowsAsync(
-       () => cert.revoke(accounts[1]),
-       "revert"
-     );
-   });
+    assertThrowsAsync(
+      () => cert.revoke(accounts[1]),
+      "revert"
+    );
+  });
 
   it("should allow set owner", async () => {
     const cert = await SimpleCertifier.deployed();
