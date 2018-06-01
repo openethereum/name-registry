925d93c4a714ea20e59f2bc3789043f9a3202577
André Silva <andre.beat@gmail.com>
2018-06-01 16:12:13 +0100

    update solidity pragma in SimpleRegistry

--- contracts/SimpleRegistry.sol
+++ contracts/SimpleRegistry.sol
@@ -14,7 +14,7 @@
 //! See the License for the specific language governing permissions and
 //! limitations under the License.
 
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.22;
 
 import "./Owned.sol";
 import "./Registry.sol";
